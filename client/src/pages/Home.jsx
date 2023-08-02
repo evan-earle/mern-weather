67b57c2efac2f56c9ff810bf97312cdcce32569d
Evan Earle <evan.earle@gmail.com>
2023-08-02 14:00:00 +0100

    08/02/23

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,56 +4,61 @@ import axios from "axios";
 
 export const Home = () => {
   const [mainCity, setMainCity] = useState("");
-  const [favouriteCityOne, setFavouriteCityOne] = useState();
-  const [favouriteCityTwo, setFavouriteCityTwo] = useState();
-  const [favouriteCityThree, setFavouriteCityThree] = useState();
-  const [search, setSearch] = useState();
+  const [favouriteCityOne, setFavouriteCityOne] = useState("");
+  const [favouriteCityTwo, setFavouriteCityTwo] = useState("");
+  const [favouriteCityThree, setFavouriteCityThree] = useState("");
+  const [search, setSearch] = useState("");
 
-  const getProfile = async () => {
+  const getCitiesFromDb = async () => {
     try {
       // call db and check what cities are favourited
       const profile = await axios.get(`/api/weather`);
       console.log(profile);
 
-      const weather = await axios.get(`/api/weather/${profile.data.mainCity}`);
-      console.log(weather);
+      const city = profile.data.mainCity;
 
+      getCity(city);
       setMainCity(profile.data.mainCity);
-      setFavouriteCityOne(profile.data.favouriteCityOne);
-      setFavouriteCityTwo(profile.data.favouriteCityTwo);
-      setFavouriteCityThree(profile.data.favouriteCityThree);
+      // setFavouriteCityOne(profile.data.favouriteCityOne);
+      // setFavouriteCityTwo(profile.data.favouriteCityTwo);
+      // setFavouriteCityThree(profile.data.favouriteCityThree);
     } catch (err) {
       console.log(err);
     }
   };
 
-  // const getWeather = async () => {
-  //   console.log(mainCity);
-  //   const weather = await axios.get(`/api/weather/${mainCity}`);
-  //   console.log(weather);
-  // };
+  const getCity = async (city) => {
+    const weather = await axios.get(`/api/weather/${city}`);
+    console.log(weather);
+  };
+
+  const getWeather = async (e) => {
+    e.preventDefault();
+    const weather = await axios.get(`/api/weather/${search}`);
+    console.log(weather);
+  };
 
   useEffect(() => {
-    getProfile();
+    getCitiesFromDb();
   }, []);
 
   return (
     <div>
-      <button type="button" onClick={getProfile}>
+      <button type="button" onClick={getCitiesFromDb}>
         {mainCity}
       </button>
-      <form onSubmit={getProfile}>
+      <form onSubmit={getWeather}>
         <input
           type="text"
-          onChange={(e) => setMainCity(e.target.value)}
-          value={mainCity}
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
         />
         <button type="submit">Search</button>
       </form>
       <Navbar />
-      {/*
-       <h1>{mainCity}</h1>
 
+      <h1>{}</h1>
+      {/*
        <button type="button" onClick={getProfile}>
          {favouriteCityOne}
        </button>
