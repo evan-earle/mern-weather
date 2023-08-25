e6ad8202e0536136e96f9eec5045d4aaa484b206
Evan Earle <evan.earle@gmail.com>
2023-08-24 21:04:36 -0300

    config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  include: ["pdfjs-dist"],
   server: {
     proxy: {
       "/api": {
--- server/server.js
+++ server/server.js
@@ -34,9 +34,9 @@ app.use((err, req, res, next) => {
 
 app.use(express.static(path.join(__dirname, "..", "client", "dist")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
+});
 
 // Start our server
 app.listen(process.env.PORT, () =>
