fe7fd8c6965fd9c68addc2b99471ce4c5b7dab49
Evan Earle <evan.earle@gmail.com>
2023-08-25 13:02:41 -0300

    .

--- server/server.js
+++ server/server.js
@@ -37,10 +37,10 @@ app.use((err, req, res, next) => {
   return res.status(status).json({ message, stack: err.stack });
 });
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "./client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
 });
 
 // Start server
