27fd6921401960b132731cfe163ab666743c09eb
Evan Earle <evan.earle@gmail.com>
2023-08-25 16:09:53 -0300

    index

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.use((err, req, res, next) => {
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
 
 // Start server
