b4226b07c9dcc8278d871988aca870a1014ad077
Evan Earle <evan.earle@gmail.com>
2023-08-24 22:42:40 -0300

    vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: "./src/index.jsx",
-    },
-  },
+
   server: {
     proxy: {
       "/api": {
