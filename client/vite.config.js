04c11c2cf521ddff56ae13e42e7f7a6c50645c36
Evan Earle <evan.earle@gmail.com>
2023-08-25 14:40:40 -0300

    try

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  build: {
+    outDir: "../server/public",
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
       "/api": {
